5bce876b21b0e678cc6f43eb1b37827051385515
mmaag01 <mmaag01@student.ethz.ch>
2026-04-17 14:41:25 +0200

    one-hot encoding, added types

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-import glob
 import torch
 import matplotlib.pyplot as plt
 from torch import nn
 import xgboost as xgb
-from data_utils import Data
-from import_data import ImportData
+from data_utils import ProcessRawData, ImportData
+#from import_data import ImportData
 
 units = {'t':'s', 'tor':'Nm', 'vel':'rad/s', 
          'pos':'rad', 'accel':'rad/s^2', 'i':'A'}
 
-def processData(main_data=True, fails_data=False, other_data=False):
-    DataProcessor = Data(main_experiments=main_data, crashed_experiments=fails_data, other_experiments=other_data)
+def convertData(main_data=True, fails_data=False, other_data=False):
+    DataProcessor = ProcessRawData(main_experiments=main_data, crashed_experiments=fails_data, other_experiments=other_data)
     DataProcessor.process_all(suffix="_exp")
 
 if __name__ == "__main__":
@@ -24,5 +23,7 @@ if __name__ == "__main__":
     other_data = importer.importOther()
     combined_data = importer.combineDatasets()
     combined_data = importer.convertUnits()
+    file_names = combined_data['file_name']
+    combined_data = combined_data.drop(columns=['file_name'])
 
    
